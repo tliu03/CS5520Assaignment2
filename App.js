4732becb3ef85b68a4bcc58bb0a7ffb3c9d467d6
王欣喏 <wangxinnuo@wangxinnuodeMacBook-Air.local>
2023-02-19 23:07:56 +0800

    created navigation skeleton

--- App.js
+++ App.js
@@ -1,16 +1,35 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import AllEntries from "./screens/AllEntries";
+import OverLimitEntries from "./screens/OverLimitEntries";
+
+function EntryContainer() {
+  const Tab = createBottomTabNavigator();
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="All Entry" component={AllEntries} />
+      <Tab.Screen name="Over Limit Entry" component={OverLimitEntries} />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   const Stack = createNativeStackNavigator();
-  const Tab = createBottomTabNavigator();
+
   return (
-    <Stack.Navigator>
-      <Stack.Screen />
-      <Stack.Screen />
-    </Stack.Navigator>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Entries"
+          component={EntryContainer}
+          options={{ headerShown: false }}
+        />
+        {/* <Stack.Screen name="" /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
