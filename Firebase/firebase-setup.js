bab4b32c7368639c573049bc3ad0de965749722c
王欣喏 <wangxinnuo@wangxinnuodeMacBook-Air.local>
2023-02-27 15:50:01 +0800

    updated babel.config.js

--- Firebase/firebase-setup.js
+++ Firebase/firebase-setup.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 import React from "react";
+import { getFirestore } from "firebase/firestore";
 import {
   apiKey,
   authDomain,
--- babel.config.js
+++ babel.config.js
@@ -3,19 +3,19 @@ module.exports = function (api) {
   return {
     presets: ["babel-preset-expo"],
     plugins: [
-      ["module:react-native-dotenv"],
-      {
-        envName: "APP_ENV",
-        moduleName: "@env",
-        path: ".env",
-        blocklist: null,
-        allowlist: null,
-        blacklist: null, // DEPRECATED
-        whitelist: null, // DEPRECATED
-        safe: false,
-        allowUndefined: true,
-        verbose: false,
-      },
+      [
+        "module:react-native-dotenv",
+        {
+          envName: "APP_ENV",
+          moduleName: "@env",
+          path: ".env",
+          blocklist: null,
+          allowlist: null,
+          safe: false,
+          allowUndefined: true,
+          verbose: false,
+        },
+      ],
     ],
   };
 };
